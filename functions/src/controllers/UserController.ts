4202781a442628bf48386df30ad6ad57f6a97357
Colinster327 <colin@getoptiwatt.com>
2024-04-12 22:12:45 -0400

    setup middleware and basic user controller

---
+++ functions/src/controllers/UserController.ts
@@ -0,0 +1,45 @@
+import { Request } from 'firebase-functions/v2/https'
+import validateFirebaseIdToken from '../middleware/validation'
+import { Response } from 'express'
+import * as admin from 'firebase-admin'
+import { USER_COLLECTION } from '..'
+
+export const getCurrentUser = async (request: Request, response: Response) => {
+  const token = await validateFirebaseIdToken(request, response)
+  const uid = token?.uid
+  if (!uid) return
+
+  try {
+    const userPromise = await admin
+      .firestore()
+      .doc(`${USER_COLLECTION}/${uid}`)
+      .get()
+    const userData = userPromise.data()
+    response.status(200).send(userData)
+  } catch {
+    response.status(404).send({ error: 'User Not Found' })
+  }
+}
+
+export const createUser = async (request: Request, response: Response) => {
+  const token = await validateFirebaseIdToken(request, response)
+  const uid = token?.uid
+  const data = request.body
+  if (!uid) return
+
+  const userInfo = {
+    name: data.name,
+    major: data.major,
+    email: data.email,
+    pid: data.pid,
+    year: data.year,
+    onboarded: data.onboarded,
+  }
+
+  try {
+    await admin.firestore().doc(`${USER_COLLECTION}/${uid}`).set(userInfo)
+    response.status(200).send(userInfo)
+  } catch (err) {
+    response.status(500).send({ error: err })
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,94 +1,15 @@
-import {onRequest} from "firebase-functions/v2/https";
-import * as admin from "firebase-admin";
-import express = require("express");
+import { onRequest } from 'firebase-functions/v2/https'
+import * as admin from 'firebase-admin'
+import express = require('express')
+import { createUser, getCurrentUser } from './controllers/UserController'
 
-const COURSE_COLLECTION = "Courses";
-const USER_COLLECTION = "Users";
+export const USER_COLLECTION = 'Users'
 
-const app = express();
-app.use(express.json());
-admin.initializeApp();
+const app = express()
+admin.initializeApp()
+app.use(express.json())
 
-app.post("/upload-course", (request, response) => {
-  const newCourse = {
-    shortName: request.body.shortName,
-    fullName: request.body.fullName,
-    college: request.body.college,
-    credits: request.body.credits,
-    description: request.body.description,
-    lab: request.body.lab,
-    semester: request.body.semester,
-    prereq: request.body.prereq,
-  };
+app.get('/get_current_user', getCurrentUser as any)
+app.post('/create_user', createUser as any)
 
-  admin
-    .firestore()
-    .collection(COURSE_COLLECTION)
-    .add(newCourse)
-    .then((res) => {
-      response.status(201).send(res.get().then((doc) => doc.data()));
-    })
-    .catch((err) => {
-      response.status(500).send({error: err});
-    });
-});
-
-app.get("/get-courses", async (request, response) => {
-  try {
-    const docs = await admin
-      .firestore()
-      .collection(COURSE_COLLECTION)
-      .listDocuments();
-    const coursesPromises = docs.map((doc) => doc.get());
-    const courses = await Promise.all(coursesPromises);
-    response.status(200).send(courses);
-  } catch (err) {
-    response.status(500).send(err);
-  }
-});
-
-app.get("/get-user", async (request, response) => {
-  try {
-    const idToken = request.headers.authorization?.split("Bearer ")[1];
-    const decodedToken = await admin.auth().verifyIdToken(idToken ?? "");
-
-    const user = await admin
-      .firestore()
-      .collection(USER_COLLECTION)
-      .doc(decodedToken.uid)
-      .get();
-    response.status(200).send(user.data());
-  } catch (err) {
-    response.status(500).send(err);
-  }
-});
-
-app.post("/set-user", async (request, response) => {
-  try {
-    const data = request.body;
-    const idToken = request.headers.authorization?.split("Bearer ")[1];
-    const decodedToken = await admin.auth().verifyIdToken(idToken ?? "");
-    console.log(decodedToken);
-
-    const newUser = {
-      name: data.name,
-      email: data.email,
-      password: data.password,
-      pid: data.pid,
-      major: data.major,
-      year: data.year,
-      onboarded: true,
-    };
-
-    await admin
-      .firestore()
-      .collection(USER_COLLECTION)
-      .doc(decodedToken.uid)
-      .set(newUser);
-    response.status(201);
-  } catch (err) {
-    response.status(500).send(err);
-  }
-});
-
-exports.api = onRequest(app);
+exports.api = onRequest(app)
