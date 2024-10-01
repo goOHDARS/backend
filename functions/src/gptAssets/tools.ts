8d4f15aa2b5f0cbd75f4bdca2d8f7c995a5185f6
Colinster327 <colin@getoptiwatt.com>
2024-09-30 23:15:03 -0400

    add AI suggestion response

--- functions/src/gptAssets/tools.ts
+++ functions/src/gptAssets/tools.ts
@@ -22,6 +22,13 @@ export const tools: ChatCompletionTool[] = [
   {
     type: 'function',
     function: {
+      name: 'getSuggestions',
+      description: 'get recommended courses for the user',
+    },
+  },
+  {
+    type: 'function',
+    function: {
       name: 'getCourseInfo',
       description: 'get the full information of a course',
       parameters: {
@@ -79,6 +86,7 @@ export const tools: ChatCompletionTool[] = [
 // a type to represent all of the possible tool calls (should match 'tools')
 type FunctionKeys =
   | 'getCourses'
+  | 'getSuggestions'
   | 'getCourseInfo'
   | 'addUserCourse'
   | 'removeUserCourse'
@@ -108,6 +116,17 @@ export const generateMessageFromToolCall = async (
       return (
         'Below are the courses you are currently enrolled in.\n\n' +
         briefs.map((el) => el.shortName).join('\n') +
+        '\n\nPlease let me know if you need anything else.'
+      )
+    },
+    getSuggestions: async () => {
+      const suggestions = (await getCoursesTool(userId)).filter(
+        // keep only suggestions
+        (el) => el.suggestion
+      )
+      return (
+        'Here are some course recommendations for next semester:\n\n' +
+        suggestions.map((el) => el.shortName).join('\n') +
         '\n\nPlease let me know if you need anything else.'
       )
     },
